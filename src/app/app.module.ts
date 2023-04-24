4f0e66b508dee9d658f6322658f067177cdaa72e
shelbyloof150 <alexis.climate@gmail.com>
2023-04-24 14:40:04 -0500

    2 files modified, 4 added, 1 deleted

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,9 +6,11 @@ import { NavbarComponent } from './navbar/navbar.component';
 import { NurseComponent } from './nurse/nurse.component';
 import { RouterModule } from '@angular/router';
 
+import { AppRoutingModule } from './app-routing.module';
+
 @NgModule({
   declarations: [AppComponent, NavbarComponent, NurseComponent],
-  imports: [BrowserModule, RouterModule],
+  imports: [BrowserModule, RouterModule, AppRoutingModule],
   providers: [],
   bootstrap: [AppComponent, NavbarComponent],
 })
