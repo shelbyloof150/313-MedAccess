79d26f0014e74e1c690051926d8d6172fa1c9094
hannahschlough <125297423+hannahschlough@users.noreply.github.com>
2023-04-24 15:21:30 -0500

    1 file modified

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,11 +7,18 @@ import { NurseComponent } from './nurse/nurse.component';
 import { RouterModule } from '@angular/router';
 import { DoctorComponent } from './doctor/doctor.component';
 import { SearchComponent } from './search/search.component';
-
+import { DatepickerComponent } from './datepicker/datepicker.component';
 import { AppRoutingModule } from './app-routing.module';
 
 @NgModule({
-  declarations: [AppComponent, NavbarComponent, NurseComponent, DoctorComponent, SearchComponent],
+  declarations: [
+    AppComponent,
+    NavbarComponent,
+    NurseComponent,
+    DoctorComponent,
+    SearchComponent,
+    DatepickerComponent,
+  ],
   imports: [BrowserModule, RouterModule, AppRoutingModule],
   providers: [],
   bootstrap: [AppComponent, NavbarComponent],
